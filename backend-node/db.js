ce81be129efdff081e3d6478f3644737a4e64718
Barbara Pereira <dra.barbaraqueiroz@gmail.com>
2026-06-10 20:17:42 -0300

    use dotenv on db connection

--- backend-node/db.js
+++ backend-node/db.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
+
 const mysql = require("mysql2");
 
+
 const conexao = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "plataforma_jogos"
+    host:  process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
 });
 
 conexao.connect((erro) => {
     if (erro) {
         console.log("Erro ao conectar:", erro);
+        throw new Error("Erro ao conectar:", erro.message);
+        
     } else {
         console.log("MySQL conectado");
     }
